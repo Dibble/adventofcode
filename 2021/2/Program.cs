3469fedf2e8682ccef1012bc9a4850a9e30feebe
James Cotterell <officer.dibble1@gmail.com>
2021-12-02 15:08:50 +0000

    Day 2 complete

---
+++ 2021/2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var sr = new StreamReader("input.txt");
+
+            var x = 0;
+            var depth = 0;
+            var aim = 0;
+
+            while (sr.Peek() >= 0)
+            {
+                var line = sr.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+
+                var splits = line.Split(' ');
+                var instruction = splits[0];
+                var value = int.Parse(splits[1]);
+
+                switch (instruction)
+                {
+                    case "forward":
+                        x += value;
+                        depth += aim * value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                }
+            }
+
+            Console.WriteLine(x);
+            Console.WriteLine(depth);
+            Console.WriteLine(x * depth);
+        }
+    }
+}
