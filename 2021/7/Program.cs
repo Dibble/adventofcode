7979dd6a03559f788a754ab7a13c5b2e742edd37
James Cotterell <officer.dibble1@gmail.com>
2021-12-08 16:15:24 +0000

    Day 7 part 2

--- 2021/7/Program.cs
+++ 2021/7/Program.cs
@@ -12,21 +12,65 @@
             var crabs = line.Split(',').Select(c => int.Parse(c)).ToList();
 
             crabs.Sort();
-            var medianPosition = crabs.Count / 2.0;
-            double median;
+            // var medianPosition = crabs.Count / 2.0;
+            // double median;
 
-            if (crabs.Count % 2 == 1)
-            {
-                median = (crabs[(int)Math.Floor(medianPosition)] + crabs[(int)Math.Ceiling(medianPosition)]) / 2;
-            }
-            else
-            {
-                median = crabs[(int)medianPosition];
-            }
-            Console.WriteLine(median);
+            // if (crabs.Count % 2 == 1)
+            // {
+            //     median = (crabs[(int)Math.Floor(medianPosition)] + crabs[(int)Math.Ceiling(medianPosition)]) / 2;
+            // }
+            // else
+            // {
+            //     median = crabs[(int)medianPosition];
+            // }
+            // Console.WriteLine(median);
 
-            var fuel = crabs.Select(c => Math.Abs(c - median)).Sum();
-            Console.WriteLine(fuel);
+            // var fuel = crabs.Select(c => Math.Abs(c - median)).Sum();
+            // Console.WriteLine(fuel);
+
+            var start = crabs.First();
+            var end = crabs.Last();
+
+            var minFuel = -1;
+
+            for (int i = start; i <= end; i++)
+            {
+                // calculate fuel usage
+                var currentFuel = 0;
+
+                for (int j = 0; j < crabs.Count; j++)
+                {
+                    var crab = crabs[j];
+                    currentFuel += Factorial(Math.Abs(crab - i));
+                    if (currentFuel > minFuel && minFuel > -1)
+                    {
+                        break;
+                    }
+                }
+
+                if (minFuel == -1)
+                {
+                    minFuel = currentFuel;
+                }
+                else
+                {
+                    minFuel = Math.Min(minFuel, currentFuel);
+                }
+            }
+
+            Console.WriteLine(minFuel);
+        }
+
+        static int Factorial (int i)
+        {
+            var result = 0;
+            while (i > 0)
+            {
+                result += i;
+                i--;
+            }
+
+            return result;
         }
     }
 }
