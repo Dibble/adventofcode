1831047f9fe24f16af85b900e360d10bb8691ac5
James Cotterell <officer.dibble1@gmail.com>
2021-12-18 21:16:25 +0000

    Day 17 not started

---
+++ 2021/17/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var sr = new StreamReader("test.txt");
+
+            var file = sr.ReadToEnd();
+            var lines = file.Split("\n").ToList();
+
+            
+        }
+    }
+}
