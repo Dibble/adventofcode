0b03b8a2b676503121798e4a33ff771d816cbdca
James Cotterell <officer.dibble1@gmail.com>
2021-12-21 14:32:07 +0000

    Day 21 part 2 WIP

--- 2021/21/Program.cs
+++ 2021/21/Program.cs
@@ -12,46 +12,57 @@
             var player1 = new Player(int.Parse(lines[0].Split(' ')[^1]));
             var player2 = new Player(int.Parse(lines[1].Split(' ')[^1]));
             var players = new[] { player1, player2 };
-
-            var dice = 1;
-            var diceRolls = 0;
-            var currentPlayer = 0;
-
-            while (!players.Any(p => p.score >= 1000))
+            var universes = new List<(int, Player[])>
             {
-                var rolls = dice;
-                dice++;
-                if (dice > 100)
-                {
-                    dice = 1;
-                }
-                rolls += dice;
-                dice++;
-                if (dice > 100)
-                {
-                    dice = 1;
-                }
-                rolls += dice;
-                dice++;
-                if (dice > 100)
-                {
-                    dice = 1;
-                }
-                diceRolls += 3;
+                (0, players)
+            };
 
-                var newPosition = players[currentPlayer].position + rolls;
-                while (newPosition > 10)
+            var wins = new[] { 0L, 0L };
+
+            while (universes.Any())
+            {
+                Console.WriteLine(universes.Count);
+                var universe = universes[0];
+                var currentPlayer = universe.Item1;
+                var player = universe.Item2[currentPlayer];
+                var nextPlayer = currentPlayer == 0 ? 1 : 0;
+
+                var newPositions = GetNewPositions(player.position);
+                newPositions.Skip(1).ToList().ForEach(pos =>
+                {
+                    var newPlayers = new[] { new Player(universe.Item2[0].position), new Player(universe.Item2[1].position) };
+                    newPlayers[currentPlayer].position = pos;
+                    newPlayers[currentPlayer].score += pos;
+                    var newUniverse = (nextPlayer, newPlayers);
+                    universes.Add(newUniverse);
+                });
+
+                player.position = newPositions[0];
+                player.score += newPositions[0];
+                universe.Item1 = nextPlayer;
+                
+                if (player.score >= 21)
+                {
+                    wins[currentPlayer]++;
+                    universes = universes.Skip(1).ToList();
+                }
+            }
+
+            Console.WriteLine(wins.Max());
+        }
+
+        static int[] GetNewPositions (int position)
+        {
+            var rolls = new[] { 3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8, 8, 9 };
+            return rolls.Select(roll =>
+            {
+                var newPosition = position + roll;
+                if (newPosition > 10)
                 {
                     newPosition -= 10;
                 }
-                players[currentPlayer].position = newPosition;
-                players[currentPlayer].score += newPosition;
-
-                currentPlayer = currentPlayer == 0 ? 1 : 0;
-            }
-
-            var loser = players.First(p => p.score < 1000);
-            Console.WriteLine($"{loser.score} * {diceRolls} = {loser.score * diceRolls}");
+                return newPosition;
+            }).ToArray();
         }
 
         struct Player
