00ac87522006845233ea4c2d89c15c08a3852526
Dibble <officer.dibble1@gmail.com>
2022-12-18 19:05:06 +0000

    2022 day 7 part 1 wip

--- 2022/rusty-rust/src/day_7.rs
+++ 2022/rusty-rust/src/day_7.rs
@@ -1,15 +1,18 @@
-use trees::{tr,Node};
+use std::fs::File;
+use std::io;
+use indextree::{Arena};
 
+#[derive(Debug)]
 struct NodeData {
   name: String,
-  size: i32,
+  size: usize
 }
 
-pub fn run(lines: io::Result<io::Lines<io::BufReader<File>>>) {
-  println!("7");
-  let mut tree = tr("/", size: 0);
-  let mut current_node = 
-
+pub fn run(lines: io::Lines<io::BufReader<File>>) {
+  let mut arena = Arena::new();
+  let root = arena.new_node(NodeData { name: String::from("/"), size: 0 });
+  let mut current_node = root;
+  
   for line in lines {
     if let Ok(ip) = line {
       if ip.starts_with("$ ") {
@@ -19,13 +22,17 @@ pub fn run(lines: io::Result<io::Lines<io::BufReader<File>>>) {
           "cd" => {
             match command_parts[2] {
               "/" => {
-                current_node = &root;
+                current_node = root;
               },
               ".." => {
-                current_node = &current_node.parent.unwrap();
+                current_node = arena.get(current_node).unwrap().parent().unwrap();
               },
               _ => {
-                current_node = current_node.children.iter().find(|x| x.name == command_parts[2]).unwrap();
+                let mut current_child = arena.get(current_node).unwrap().first_child().unwrap();
+                while arena.get(current_child).unwrap().get().name != command_parts[2] {
+                  current_child = arena.get(current_child).unwrap().next_sibling().unwrap();
+                }
+                current_node = current_child;
               }
             }
           },
@@ -36,7 +43,33 @@ pub fn run(lines: io::Result<io::Lines<io::BufReader<File>>>) {
         }
       } else {
         // output
+        let output_parts = ip.split(' ').collect::<Vec<&str>>();
+
+        if ip.starts_with("dir ") {
+          // directory
+          // new node
+          let dir = arena.new_node(NodeData { name: String::from(output_parts[1]), size: 0 });
+          // append to current
+          current_node.append(dir, &mut arena);
+        } else {
+          // file with size
+          let size = output_parts[0].parse::<usize>().unwrap();
+          let file = arena.new_node(NodeData { name: String::from(output_parts[1]), size: size });
+          current_node.append(file, &mut arena);
+        }
       }
     }
   }
+
+  let mut iter = root.reverse_traverse(&arena);
+  let mut node = iter.next();
+  while node != None {
+    if node.size == 0 {
+      let mut size = 0;
+      
+    }
+    node = iter.next();
+  }
+
+  println!("{:?}", arena);
 }
